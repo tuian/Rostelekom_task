a17e6fed92d02d1d8af39b66fb114d2da86f899b
Gleb Lisov <barabashka.zzz@gmail.com>
2017-07-18 02:24:14 +0300

    last changes

--- get_server/run.py
+++ get_server/run.py
@@ -1,9 +1,6 @@
 #!flask/bin/python
 from get_server_app import get_server
 
-# context = ('/etc/letsencrypt/live/iocs.tk/fullchain.pem', '/etc/letsencrypt/live/iocs.tk/privkey.pem')
+context = ('./certificate/fullchain.pem', './certificate/privkey.pem')
 
-
-# get_server.run(debug=None, host='0.0.0.0', port='443', ssl_context=context)
-get_server.run(host='0.0.0.0', port='80')
-
+get_server.run(debug=None, host='0.0.0.0', port='443', ssl_context=context)
